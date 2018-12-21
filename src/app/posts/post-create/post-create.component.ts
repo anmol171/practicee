8c171a5d4809779998224306329913161c46f20d
shubham <shubham.agarwal@polestarllp.com>
2018-12-21 20:10:11 +0530

    folder structure frontend

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Post } from '../post.model';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -11,17 +11,15 @@ export class PostCreateComponent implements OnInit {
 
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
-  constructor() { }
+  constructor(public postsService: PostsService) { }
 
   ngOnInit() {
   }
 
   onAddPost(form: NgForm) {
-    const post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -12,12 +13,15 @@ export class PostListComponent implements OnInit {
   //   {title: 'First post', content: 'This is First Post'},
   //   {title: 'Second post', content: 'This is Second Post'},
   //   {title: 'Third post', content: 'This is Third Post'}
-    
+
   // ];
-  @Input() posts: Post[] = [];
-  constructor() { }
+  posts: Post[] = [];
+
+  constructor(public postsService: PostsService) { }
 
   ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener().subscribe();
   }
 
 }
